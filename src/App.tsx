c48927e6c9bd200250cbc34dbb668979a2f1db67
michaelkmalak <cairoindiefc@gmail.com>
2026-07-10 22:29:37 +0300

    Add /crew redirect route

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const App = () => (
       <Route path="/" element={<Index />} />
       <Route path="/join" element={<ExternalRedirect to="https://forms.gle/CKv3xLR3ZjubuHdV6" />} />
       <Route path="/actors" element={<ExternalRedirect to="https://forms.gle/fFTMXbxYG65brXY88" />} />
+      <Route path="/crew" element={<ExternalRedirect to="https://forms.gle/a7jBfNZzgkZdXi6Z9" />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
