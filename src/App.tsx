b1fe6bd76f22ad93e4436881427ec270b3bb4155
michaelkmalak <cairoindiefc@gmail.com>
2026-07-14 16:32:26 +0300

    Rename /crew to /help-us, add /film-distribution redirect

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ const App = () => (
       <Route path="/" element={<Index />} />
       <Route path="/join" element={<ExternalRedirect to="https://forms.gle/CKv3xLR3ZjubuHdV6" />} />
       <Route path="/actors" element={<ExternalRedirect to="https://forms.gle/fFTMXbxYG65brXY88" />} />
-      <Route path="/crew" element={<ExternalRedirect to="https://forms.gle/a7jBfNZzgkZdXi6Z9" />} />
+      <Route path="/help-us" element={<ExternalRedirect to="https://forms.gle/a7jBfNZzgkZdXi6Z9" />} />
+      <Route path="/film-distribution" element={<ExternalRedirect to="https://forms.gle/Nhn4YGBoQw5Mav6V6" />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
